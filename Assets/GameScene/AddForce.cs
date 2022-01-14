6a705d88301fe100debeae7725ac2fb16b3108d0
kurama13717 <2200460@ecccomp.com>
2022-01-14 14:37:18 +0900

    tennmetu

--- Assets/GameScene/AddForce.cs
+++ Assets/GameScene/AddForce.cs
@@ -14,6 +14,7 @@ public class AddForce : MonoBehaviour
     public float scalelimit;    // 大きさ制限
     [SerializeField]
     float LimitSpeed;
+    private bool isDamage { get; set; }
 
     void Start()
     {
@@ -21,6 +22,9 @@ public class AddForce : MonoBehaviour
         this.rb.velocity = new Vector3(0, 0, 0);
 
     }
+
+    float totalTime = 0.0f;
+    float fadeCt = 0;
 
     void FixedUpdate()
     {
@@ -46,10 +50,33 @@ public class AddForce : MonoBehaviour
             rb.velocity = new Vector3(rb.velocity.x / 1.2f, 10.0f, rb.velocity.z / 1.2f);
         }
 #endif
-
         this.transform.localScale += new Vector3(increase, increase, increase);     // スケール増加
         if (this.transform.localScale.x > scalelimit)
             this.transform.localScale = new Vector3(scalelimit, scalelimit, scalelimit);
+
+        
+
+        if (isDamage)   // ダメージを受けた時
+        {
+            //float level = Mathf.Sin(Time.deltaTime * 6.0f) / 2 + 0.5f;
+
+            fadeCt += 2;
+            float level = Mathf.Sin(fadeCt) / 2.0f + 0.5f;
+            var renderer = this.gameObject.GetComponent<Renderer>();
+            var color = renderer.material.color;
+            color.a = level;
+            renderer.material.color = color;
+
+            totalTime += Time.deltaTime;
+            if (totalTime >= 3.0f) {
+                //this.gameObject.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f, 1f);
+                color.a = 1.0f;
+                renderer.material.color = color;
+                totalTime = 0.0f;
+                isDamage = false;
+            }
+        }
+
         Debug.Log(rb.velocity.magnitude);
     }
 
@@ -66,5 +93,27 @@ public class AddForce : MonoBehaviour
             myTransform.position = pos;  // 座標を設定
         }
                 
+    }
+
+    void OnTriggerEnter(Collider collider)  // 壁との当たり判定
+    {
+        Transform myTransform = this.transform; // transformを取得
+        if (collider.gameObject.name == "Wall")
+        {
+            isDamage = true;
+            this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);  // 大きさを初期値に戻す
+            //StartCoroutine(OnDamage());
+        }
+    }
+
+    public void OnDamage()
+    {
+
+        //yield return new WaitForSeconds(3.0f);
+
+        // 通常状態に戻す
+        isDamage = false;
+        this.gameObject.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f, 1f);
+
     }
 }
