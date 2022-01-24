203ca8ef3daabc3b267531836523d68649b69f68
wata0505 <95832079+wata0505@users.noreply.github.com>
2022-01-24 14:50:37 +0900

    a

---
+++ Assets/GameScene/Auido.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Auido : MonoBehaviour
+{
+	public class PlayAudio : MonoBehaviour
+	{
+		// 音データの再生装置を格納する変数
+		private AudioSource audio;
+
+		// 音データを格納する変数（Inspectorタブからも値を変更できるようにする）
+		[SerializeField]
+		private AudioClip sound;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			// ゲームスタート時にAudioSource（音再生装置）のコンポーネントを加える
+			audio = gameObject.AddComponent<AudioSource>();
+
+		}
+
+		/// <summary>
+		/// 衝突した時
+		/// </summary>
+		/// <param name="collision"></param>
+		void OnCollisionEnter(Collision collision)
+		{
+			// 衝突した相手にPlayerタグが付いているとき
+			if (collision.gameObject.tag == "Sphere")
+			{
+				// 音（sound）を一度だけ（PlayOneShot）再生する
+				audio.PlayOneShot(sound);
+			}
+		}
+	}
+}
