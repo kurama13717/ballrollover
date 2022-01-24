a6761e7f94610337fc052d8acc2f9f4fee6ccf3e
todo48 <2200300@ecccomp.com>
2022-01-24 16:38:23 +0900

    sa

--- Assets/Retrygame2.cs
+++ Assets/Retrygame2.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
-public class WitchRetry : MonoBehaviour
+public class Retrygame2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -23,7 +21,8 @@ public class WitchRetry : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0))
         {
-            SceneManager.LoadScene("GameScene1");
-        }    
+            SceneManager.LoadScene("GameScene5");
+        }
     }
+
 }
